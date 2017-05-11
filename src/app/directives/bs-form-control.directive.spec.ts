26e419532af69e015802a9e166e2bbca4e4d59c8
Marcos Vinícius Rubido Sauda <marcosauda@hotmail.com>
2017-05-11 13:28:53 +0200

    src folder deleted and dist ignored

--- src/app/directives/bs-form-control.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BsFormControlDirective } from './bs-form-control.directive';
-
-describe('BsFormControlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BsFormControlDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BsFormControlDirective } from "app/directives/bs-form-control.directive";
-
-@NgModule({
-  declarations: [BsFormControlDirective],
-  exports: [BsFormControlDirective]
-})
-export class DirectivesModule { }
