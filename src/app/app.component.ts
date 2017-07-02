4406da7ae1f927265515f9709bd139dec61ad70d
Marcos Vinícius Rubido Sauda <marcosauda@hotmail.com>
2017-07-02 21:59:36 +0200

    Design correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit, isDevMode } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 import { GoogleAnalyticsService } from 'app/services/google-analytics-events.service';
 
@@ -9,16 +9,36 @@ import { GoogleAnalyticsService } from 'app/services/google-analytics-events.ser
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  errorText: any;
+  error: boolean = false;
   /**
    * Create a App Component
    * @param router - Router provider service
    */
-  constructor(public router: Router, private googleAnalyticsService: GoogleAnalyticsService) {
-    googleAnalyticsService.emitPageView();
+  constructor(public router: Router, private route: ActivatedRoute, private googleAnalyticsService: GoogleAnalyticsService) { }
+
+  ngOnInit() {
+    this.googleAnalyticsService.emitPageView();
+
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        googleAnalyticsService.emitPageView(event.urlAfterRedirects);
+        this.googleAnalyticsService.emitPageView(event.urlAfterRedirects);
+      }
+    });
+
+    this.route.queryParams.subscribe(params => {
+      if (isDevMode()) {
+        console.log(params);
+      }
+      if (params['error'] === "404") {
+        this.errorText = "The page you requested cannot be found!";
+        this.error = true;
+      } else if (params['error']) {
+        this.error = true;
+        this.errorText = params['error'];
+      } else {
+        this.error = false;
       }
     });
   }
